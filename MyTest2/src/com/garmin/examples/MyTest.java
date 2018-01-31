28a608892c8c07e0925a1b4f948dec07d3637169
smnehal <35955050+smnehal@users.noreply.github.com>
2018-01-31 10:38:46 +0000

    Add files via upload

---
+++ MyTest2/src/com/garmin/examples/MyTest.java
@@ -0,0 +1,11 @@
+package com.garmin.examples;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello how are you.. ?");
+	}
+
+}
